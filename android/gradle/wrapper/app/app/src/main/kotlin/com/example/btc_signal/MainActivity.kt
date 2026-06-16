69a57f8b9320e3b3a6cc0731181677cbe5a55654
zhaowen824 <zwen01758@gmail.com>
2026-06-16 21:32:48 +0800

    Delete android/gradle/wrapper/app directory

--- android/gradle/wrapper/app/app/src/main/kotlin/com/example/btc_signal/MainActivity.kt
+++
@@ -1,2 +0,0 @@
-// Legacy nested MainActivity disabled to avoid duplicate Android modules being detected by CI.
-// The actual MainActivity is in android/app/src/main/kotlin/com/example/btc_signal/MainActivity.kt
